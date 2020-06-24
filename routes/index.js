898414ae8a5b41445271eb98f189f45c02a656e3
Laetitia Langlois <weblaetitia@hotmail.com>
2020-06-24 12:03:19 +0200

    Add functionalities: dynamicaly adding and deleting bikes to the cart

--- routes/index.js
+++ routes/index.js
@@ -48,54 +48,20 @@ router.get('/', function(req, res, next) {
 });
 
 // Mon panier
-
-dataCardBikes = [
-  {
-  id : 1,
-  name : 'BIKO45',
-  price : 679,
-  photo: '/images/bike-1.jpg',
-  quantity: 1
-},
-{
-  id : 2,
-  name : 'ZOOK7',
-  price : 799,
-  photo: '/images/bike-2.jpg',
-  quantity: 5
-},
-{
-  id : 3,
-  name : 'LIKO89',
-  price : 839,
-  photo: '/images/bike-3.jpg',
-  quantity: 3
-}, 
-{
-  id : 4,
-  name : 'GEWO8',
-  price : 1249,
-  photo: '/images/bike-4.jpg',
-  quantity: 0
-},
-{
-  id : 5,
-  name : 'KIWIT',
-  price : 899,
-  photo: '/images/bike-5.jpg',
-  quantity: 1
-},
-{
-  id : 6,
-  name : 'NASAY',
-  price : 1399,
-  photo: '/images/bike-6.jpg',
-  quantity: 2
-}];
+dataCardBikes = [];
 
 /* GET shopping-cart page. */
 router.get('/shop', function(req, res, next) {
-  res.render('shop', {dataCardBikes:dataCardBikes});
+  dataCardBikes.push(req.query)
+  res.render('shop', {dataCardBikes:dataCardBikes} )
 });
+
+/* GET delete-shop page. */
+router.get('/delete-shop', function(req, res, next) {
+   var objectToDelete = req.query.delete
+   dataCardBikes.splice(objectToDelete,1)
+   res.render('shop', {dataCardBikes:dataCardBikes} )
+});
+
 
 module.exports = router;
